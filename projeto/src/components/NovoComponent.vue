ad956965fcaa60c469c4a2c7bac76e9cb46feca5
Saikim <kim_romeu@hotmail.com>
2022-03-16 09:20:56 -0300

    novo component

---
+++ projeto/src/components/NovoComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class ="novo-component">
+        <h1>Novo Componente</h1>
+        <p>Teste</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NovoComponent'
+}
+</script>
+
+<style scoped>
+
+</style>
