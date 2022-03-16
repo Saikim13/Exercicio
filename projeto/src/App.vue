f7c0fd12e408ea076b140d0b93f359cf85c93a15
Saikim <kim_romeu@hotmail.com>
2022-03-16 10:14:53 -0300

    TesteEvento

--- projeto/src/App.vue
+++ projeto/src/App.vue
@@ -3,6 +3,7 @@
     <NovoComponent/>
     <TesteIf/>
     <TesteFor/>
+    <TesteEvento/>
 
   </div>
 </template>
@@ -11,13 +12,15 @@
 import NovoComponent from './components/NovoComponent.vue'
 import TesteIf from './components/TesteIf.vue'
 import TesteFor from './components/TesteFor.vue'
+import TesteEvento from './components/TesteEvento.vue'
 
 export default {
   name: 'App',
   components: {
     NovoComponent,
     TesteIf,
-    TesteFor
+    TesteFor,
+    TesteEvento
   }
 }
 </script>
---
+++ projeto/src/components/TesteEvento.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="teste-evento">
+        <h1 v-on:click="deixarMaiuscula"> {{ mensagem }} </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TesteEvento',
+    data() {
+        return {
+            mensagem: 'O rato roeu a roupa do rei de roma'
+        }
+    },
+    methods: {
+        deixarMaiuscula() {
+            this.mensagem = this.mensagem.toUpperCase();
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
